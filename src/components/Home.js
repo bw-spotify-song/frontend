e06414e3ec61e3c4c3ed6a27e9f2c9884795e127
William Herman <williamajherman@gmail.com>
2020-08-25 16:44:29 -0400

    added album cards

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,22 @@
 import React from 'react'
 import {useState, useEffect} from "react";
 import { axiosWithSpotify, getToken } from "../utils/example";
+import styled from 'styled-components';
 
-///spotify IDS
-  
+// STYLED COMPONENTS
+    const StyledAlbums = styled.div`
+
+    display: flex;
+    flex-wrap: wrap;
+    align-items: center;
+    justify-content: center;
+
+    .albumCard {
+        margin: 1%;
+    }
+
+    `
+  ///spotify IDS
   const spotifyID = [
     "76tg7ywRlzuLgavgQNV6s1", // Gimme A Track
     "7F6VUTLu4qtAZZyy1lCf63", // Growing Pains
@@ -19,7 +32,7 @@ import { axiosWithSpotify, getToken } from "../utils/example";
       axiosWithSpotify()
         .get(`https://api.spotify.com/v1/tracks/?ids=${spotifyID.join(",")}`)
         .then((res) => {
-          //console.log(res.data)
+          console.log(res.data)
           setList(res.data.tracks)
         })
     }, [])
@@ -27,13 +40,17 @@ import { axiosWithSpotify, getToken } from "../utils/example";
     return (
         <div>
             <h6>This is an example of Spotify API usage</h6>
+        <StyledAlbums>
         {list.map((track) => {
           return (
-            <div key={track.id}>
+            <div className = "albumCard" key={track.id}>
               <img src={track.album.images[1].url} alt="cover"></img>
-            </div>
+              <a href = {track.external_urls.spotify}>{track.artists[0].name} - {track.name}</a>
+        </div>
           )
         })}
+        </StyledAlbums>
+        
       </div>
     )
   }
