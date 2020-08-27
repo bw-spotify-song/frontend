2687d3af63087c0ca5b2d419f76fe9542bbff4d3
Yue Su <34404957+yue-su@users.noreply.github.com>
2020-08-27 01:07:58 -0300

    fixed the user edit form issue
    changed lastname to lastName in the form value.

--- src/components/user/Admin.js
+++ src/components/user/Admin.js
@@ -1,52 +1,53 @@
-import React, { useEffect } from 'react'
+import React, { useEffect } from "react"
 //import { axiosWithAuth } from '../../utils/axiosWithAuth'
-import { fetchUserList, deleteUser, editUser } from '../../store/actions/appActions'
-import { useSelector, useDispatch, connect } from 'react-redux'
-import {Link} from 'react-router-dom'
-import { Box } from '@material-ui/core'
-
+import {
+  fetchUserList,
+  deleteUser,
+  editUser,
+} from "../../store/actions/appActions"
+import { useSelector, useDispatch, connect } from "react-redux"
+import { Link } from "react-router-dom"
+import { Box } from "@material-ui/core"
 
 const Admin = (props) => {
+  const userList = useSelector((state) => state.userList)
+  const dispatch = useDispatch()
 
-    const userList = useSelector(state => state.userList)
-    const dispatch = useDispatch()
+  // useEffect(() => {
+  //     axiosWithAuth().post('/auth/login', {
+  //         "email": "tester@tester.com",
+  //         "password": "password"
+  //     }).then(res => {
+  //         console.log(res.data)
+  //         localStorage.setItem("token", res.data.token)
+  //     })
+  // }, [])
 
-    // useEffect(() => {
-    //     axiosWithAuth().post('/auth/login', {
-    //         "email": "tester@tester.com",
-    //         "password": "password"
-    //     }).then(res => {
-    //         console.log(res.data)
-    //         localStorage.setItem("token", res.data.token)
-    //     })
-    // }, [])
+  useEffect(() => {
+    dispatch(fetchUserList())
+  }, [dispatch])
 
-    useEffect(() => {
-        dispatch(fetchUserList())
-        
-    },[dispatch])
+  return (
+    <Box align="center" m={2}>
+      {userList.map((user) => {
+        return (
+          <div key={user.id}>
+            {/* <p>{user.id}</p> */}
+            <h3>{user.firstName}</h3>
+            <h3>{user.lastName}</h3>
+            <p>{user.email}</p>
+            <Link to={`/user/${user.id}`}>
+              <button onClick={() => props.editUser(user.id)}>Edit User</button>
+            </Link>
 
-    return (
-        
-        <Box align='center' m={2}>
-            {
-                userList.map(user => {
-                    return (
-                        <div key={user.id}>
-                            {/* <p>{user.id}</p> */}
-                            <h3>{user.firstName}</h3>
-                            <p>{user.email}</p>
-                            <Link to={`/user/${user.id}`}>
-                                <button onClick={() => props.editUser(user.id)}>Edit User</button>
-                            </Link>
-                            
-                            <button onClick={() => props.deleteUser(user.id)}>Delete User</button>
-                        </div>
-                )})
-        }
-            </Box>
-            
-    )
+            <button onClick={() => props.deleteUser(user.id)}>
+              Delete User
+            </button>
+          </div>
+        )
+      })}
+    </Box>
+  )
 }
 
-export default connect(null, {deleteUser, editUser}) (Admin)
+export default connect(null, { deleteUser, editUser })(Admin)
